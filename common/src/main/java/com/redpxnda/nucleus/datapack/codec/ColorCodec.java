78fc985f81673c1a035c971767495ba962fb4133
Panda <grames8888@gmail.com>
2023-07-28 20:05:09 -0400

    color competence

--- common/src/main/java/com/redpxnda/nucleus/datapack/codec/ColorCodec.java
+++ common/src/main/java/com/redpxnda/nucleus/datapack/codec/ColorCodec.java
@@ -4,53 +4,42 @@ import com.mojang.datafixers.util.Pair;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
 import com.mojang.serialization.DynamicOps;
+import com.mojang.serialization.MapLike;
 import com.redpxnda.nucleus.Nucleus;
+import com.redpxnda.nucleus.util.Color;
 import net.minecraft.util.FastColor;
 
 import java.util.List;
 import java.util.stream.Stream;
 
-public class ColorCodec implements Codec<Integer> {
+public class ColorCodec implements Codec<Color> {
     public static final ColorCodec INSTANCE = new ColorCodec();
 
     protected ColorCodec() {}
 
     @Override
-    public <T> DataResult<Pair<Integer, T>> decode(DynamicOps<T> ops, T input) {
+    public <T> DataResult<Pair<Color, T>> decode(DynamicOps<T> ops, T input) {
         DataResult<String> potentialString = ops.getStringValue(input);
         if (potentialString.result().isPresent()) {
             String hex = potentialString.result().get();
-            int color;
-            if (hex.isEmpty()) color = FastColor.ARGB32.color(255, 255, 255, 255);
-            else {
-                long longValue = Long.parseLong(hex, 16);
-                color = (int) (longValue & 0xffffffffL);
-            }
-
-            return DataResult.success(Pair.of(color, input));
+            return DataResult.success(Pair.of(new Color(hex), input));
         }
 
-        DataResult<Stream<T>> potentialStream = ops.getStream(input);
-        if (potentialStream.result().isPresent()) {
-            List<Integer> ints = potentialStream.result().get()
-                    .map(element -> {
-                        return ops.getNumberValue(element)
-                                .getOrThrow(false, s -> Nucleus.LOGGER.error("Failed to create number from '" + element + "' for use in a color! -> " + s)).intValue();
-                    }).toList();
-            Integer color = null;
-            switch (ints.size()) {
-                case 3 -> color = FastColor.ARGB32.color(255, ints.get(0), ints.get(1), ints.get(2));
-                case 4 -> color = FastColor.ARGB32.color(ints.get(3), ints.get(0), ints.get(1), ints.get(2));
-            }
-            return color == null ?
-                    DataResult.error(() -> "RGB/RGBA colors must contain exactly 3 or 4 colors!") :
-                    DataResult.success(Pair.of(color, input));
+        DataResult<MapLike<T>> potentialMap = ops.getMap(input);
+        if (potentialMap.result().isPresent()) {
+            MapLike<T> map = potentialMap.result().get();
+            int r = ops.getNumberValue(map.get("r")).getOrThrow(false, s -> Nucleus.LOGGER.error("Invalid number used for color's r value! '" + map.get("r") + "'")).intValue();
+            int g = ops.getNumberValue(map.get("g")).getOrThrow(false, s -> Nucleus.LOGGER.error("Invalid number used for color's g value! '" + map.get("g") + "'")).intValue();
+            int b = ops.getNumberValue(map.get("b")).getOrThrow(false, s -> Nucleus.LOGGER.error("Invalid number used for color's b value! '" + map.get("b") + "'")).intValue();
+            int a = ops.getNumberValue(map.get("a"), 255).intValue();
+            return DataResult.success(Pair.of(new Color(r, g, b, a), input));
         }
+
         return DataResult.error(() -> "Could not create a color from ColorCodec! Not a valid format -> " + input);
     }
 
     @Override
-    public <T> DataResult<T> encode(Integer input, DynamicOps<T> ops, T prefix) {
-        return DataResult.success(ops.createInt(input));
+    public <T> DataResult<T> encode(Color input, DynamicOps<T> ops, T prefix) {
+        return DataResult.success(ops.createString(input.hex()));
     }
 }
---
+++ common/src/main/java/com/redpxnda/nucleus/util/Color.java
@@ -0,0 +1,76 @@
+package com.redpxnda.nucleus.util;
+
+import net.minecraft.util.FastColor;
+
+public class Color {
+    public int r;
+    public int g;
+    public int b;
+    public int a;
+
+    public Color(int r, int g, int b, int a) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.a = a;
+    }
+
+    public Color(int argb) {
+        r = FastColor.ARGB32.red(argb);
+        b = FastColor.ARGB32.blue(argb);
+        g = FastColor.ARGB32.green(argb);
+        a = FastColor.ARGB32.alpha(argb);
+    }
+
+    public Color(String hex, boolean alphaBefore) {
+        if (hex.startsWith("#")) hex = hex.substring(1);
+        if (alphaBefore) {
+            a = Integer.valueOf(hex.substring(0, 2), 16);
+            r = Integer.valueOf(hex.substring(2, 4), 16);
+            g = Integer.valueOf(hex.substring(4, 6), 16);
+            b = Integer.valueOf(hex.substring(6, 8), 16);
+        } else {
+            r = Integer.valueOf(hex.substring(0, 2), 16);
+            g = Integer.valueOf(hex.substring(2, 4), 16);
+            b = Integer.valueOf(hex.substring(4, 6), 16);
+            if (hex.length() > 6)
+                a  = Integer.valueOf(hex.substring(6, 8), 16);
+        }
+    }
+    public Color(String hex) {
+        this(hex, false);
+    }
+
+    public float redAsFloat() {
+        return r/255f;
+    }
+    public float greenAsFloat() {
+        return g/255f;
+    }
+    public float blueAsFloat() {
+        return b/255f;
+    }
+    public float alphaAsFloat() {
+        return a/255f;
+    }
+
+    public int argb() {
+        return FastColor.ARGB32.color(a, r, g, b);
+    }
+    public int abgr() {
+        return FastColor.ARGB32.color(a, b, g, r);
+    }
+
+    public String hex() {
+        return String.format("%02x%02x%02x", r, g, b);
+    }
+    public String hexWithPrefix() {
+        return String.format("#%02x%02x%02x", r, g, b);
+    }
+    public int hexInt() {
+        int rgb = r;
+        rgb = (rgb << 8) + g;
+        rgb = (rgb << 8) + b;
+        return rgb;
+    }
+}
