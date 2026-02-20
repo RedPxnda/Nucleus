79cf9216bd9e86cfd06bcf41727f3d2600cc429e
Panda <grames8888@gmail.com>
2026-02-20 11:27:47 -0500

    Added ability to override registry for a field

--- registration/registration-forge/src/main/java/com/redpxnda/nucleus/registration/forge/RegistryAnalyzerImpl.java
+++ registration/registration-forge/src/main/java/com/redpxnda/nucleus/registration/forge/RegistryAnalyzerImpl.java
@@ -12,6 +12,8 @@ import net.minecraft.util.Tuple;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -35,12 +37,37 @@ public class RegistryAnalyzerImpl {
 
                 Class<?> type = field.getType();
                 ResourceKey<?> reg = null;
-                for (Map.Entry<Class<?>, Registry<?>> entry : MiscUtil.objectsToRegistries.entrySet()) {
-                    if (entry.getKey().isAssignableFrom(type)) {
-                        reg = entry.getValue().key();
-                        break;
+                if (id.registry().equals("default")) {
+                    for (Map.Entry<Class<?>, Registry<?>> entry : MiscUtil.objectsToRegistries.entrySet()) {
+                        if (entry.getKey().isAssignableFrom(type)) {
+                            reg = entry.getValue().key();
+                            break;
+                        }
+                    }
+                } else {
+                    try {
+                        Field override = cls.getField(id.registry());
+                        override.setAccessible(true);
+                        if (Modifier.isStatic(override.getModifiers())) {
+                            Type overrideType = override.getGenericType();
+                            if (overrideType instanceof ParameterizedType pt && pt.getRawType().equals(Supplier.class)) {
+                                Type[] subs = pt.getActualTypeArguments();
+                                if (subs.length > 0 && subs[0] instanceof ParameterizedType inner && inner.getRawType().equals(Registry.class)) {
+                                    reg = ((Supplier<Registry>)override.get(null)).get().key();
+                                } else {
+                                    RegistryAnalyzer.LOGGER.warn("Field '{}' used as an override for id '{}' in registry class '{}' is not a Supplier<Registry<...>>!", id.registry(), id.value(), cls.getSimpleName());
+                                }
+                            } else {
+                                RegistryAnalyzer.LOGGER.warn("Field '{}' used as an override for id '{}' in registry class '{}' is not a Supplier!", id.registry(), id.value(), cls.getSimpleName());
+                            }
+                        } else {
+                            RegistryAnalyzer.LOGGER.warn("Field '{}' used as an override for id '{}' in registry class '{}' is not static!", id.registry(), id.value(), cls.getSimpleName());
+                        }
+                    } catch (Exception e) {
+                        RegistryAnalyzer.LOGGER.warn("Failed to get field '" + id.registry() + "' used as a registry override for id '" + id.value() + "' of registry class '" + cls.getSimpleName() + "'! -> ", e);
                     }
                 }
+
                 if (reg == null) {
                     RegistryAnalyzer.LOGGER.warn("Failed to find registry type for type '{}' in '{}'!", type.getSimpleName(), cls.getSimpleName());
                     continue;
--- test/test-common/src/main/java/com/redpxnda/nucleus/test/TestRegistries.java
+++ test/test-common/src/main/java/com/redpxnda/nucleus/test/TestRegistries.java
@@ -3,7 +3,11 @@ package com.redpxnda.nucleus.test;
 import com.redpxnda.nucleus.registration.ItemGroupCreator;
 import com.redpxnda.nucleus.registration.RegistryId;
 import dev.architectury.registry.CreativeTabRegistry;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.item.CompassItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
@@ -11,7 +15,11 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 
+import java.util.function.Supplier;
+
 public class TestRegistries {
+    public static final Supplier<Registry<?>> myCustomRegistry = () -> BuiltInRegistries.SOUND_EVENT;
+
     @RegistryId("cool_item")
     public static final CompassItem testItem = new CompassItem(new Item.Properties());
 
@@ -23,4 +31,7 @@ public class TestRegistries {
             CreativeTabRegistry.create(Component.literal("YOOO WASSGOOD"), () -> Items.STICK.getDefaultInstance()),
             testItem
     );
+
+    @RegistryId(value = "cool_sound", registry = "myCustomRegistry")
+    public static final Object mySound = SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath("nucleus", "cool_sound"));
 }
